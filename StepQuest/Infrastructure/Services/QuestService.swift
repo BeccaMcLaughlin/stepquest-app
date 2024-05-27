531cee04d4b575fbd4e7456c59273f2ad6f24f43
Tristan <daelune@gmail.com>
2024-05-27 21:28:11 +0100

    Refactor new quest

--- StepQuest/Infrastructure/Services/QuestService.swift
+++ StepQuest/Infrastructure/Services/QuestService.swift
@@ -9,30 +9,43 @@ import Foundation
 
 class QuestService {
     func FetchNewQuest(difficulty: QuestDifficulty, completion: @escaping (Result<Quest, Error>) -> Void) {
-        guard let url = URL(string: "https://0.0.0.0:8080/new?difficulty=\(difficulty.rawValue)") else {
-             completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
-             return
-         }
-
-         let task = URLSession.shared.dataTask(with: url) { data, response, error in
-             if let error = error {
-                 completion(.failure(error))
-                 return
-             }
-
-             guard let data = data else {
-                 completion(.failure(NSError(domain: "No data", code: 0, userInfo: nil)))
-                 return
-             }
-
-             do {
-                 let quest = try JSONDecoder().decode(Quest.self, from: data)
-                 completion(.success(quest))
-             } catch {
-                 completion(.failure(error))
-             }
-         }
-
-         task.resume()
+        guard let url = URL(string: "https://0.0.0.0:8443/new?difficulty=\(difficulty.rawValue)") else {
+            completion(.failure(NSError(domain: "Invalid URL", code: 0, userInfo: nil)))
+            return
+        }
+        
+        let task = URLSession.shared.dataTask(with: url) { data, response, error in
+            if let error = error {
+                completion(.failure(error))
+                return
+            }
+            
+            guard let data = data else {
+                completion(.failure(NSError(domain: "No data", code: 0, userInfo: nil)))
+                return
+            }
+            
+            do {
+                let quest = try JSONDecoder().decode(Quest.self, from: data)
+                completion(.success(quest))
+            } catch {
+                completion(.failure(error))
+            }
+        }
+        
+        task.resume()
+    }
+    
+    static func fetchAndStoreQuest(difficulty: QuestDifficulty, questStore: QuestStore, questService: QuestService, completion: @escaping () -> Void) {
+        questService.FetchNewQuest(difficulty: difficulty) { result in
+                switch result {
+                case .success(let newQuest):
+                    questStore.setCurrentQuest(newQuest)
+                    completion()
+                case .failure(let error):
+                    // Handle the error, e.g., log it or display an alert
+                    print("Error fetching new quest: \(error)")
+                }
+            }
     }
 }
--- StepQuest/Screens/NewQuest.swift
+++ StepQuest/Screens/NewQuest.swift
@@ -1,32 +1,50 @@
-//
-//  NewQuest.swift
-//  StepQuest
-//
-//  Created by Tristan McLaughlin on 27/05/2024.
-//
-
 import SwiftUI
 
 struct NewQuest: View {
+    @EnvironmentObject var questStore: QuestStore
+    @State private var isLoading = false
+    @State private var navigateToHome = false
+    private let questService = QuestService()
+
     var body: some View {
         ZStack {
             Background()
-            CharacterWithText(
-                character: CharacterType.questgiver,
-                text: """
-        Hey! I’m the Quest Giver. Let me know
-        what sort of quest you want. The Townsfolk are always looking for someone to complete their errands!
-        """,
-                buttons: [
-                    ButtonInfo(text: "Give me a less strenuous quest", action: { print("Selected option: Yes, I will help!") }),
-                    ButtonInfo(text: "Give me a difficult quest", action: { print("Selected option: Yes, I will help!") }),
-                    ButtonInfo(text: "Give me a regular quest", action: { print("Selected option: No, I can't right now.") })
-                ]
-            )
+
+            if isLoading {
+                ProgressView("Fetching quest...")
+            } else {
+                CharacterWithText(
+                    character: CharacterType.questgiver,
+                    text: """
+                    Hey! I’m the Quest Giver. Let me know
+                    what sort of quest you want. The Townsfolk are always looking for someone to complete their errands!
+                    """,
+                    buttons: [
+                        ButtonInfo(text: "Give me a less strenuous quest", action: { fetchQuest(difficulty: .easy) }),
+                        ButtonInfo(text: "Give me a difficult quest", action: { fetchQuest(difficulty: .hard) }),
+                        ButtonInfo(text: "Give me a regular quest", action: { fetchQuest(difficulty: .medium) })
+                    ]
+                )
+            }
+        }
+        .navigationDestination(isPresented: $navigateToHome) {
+            Home(currentSteps: 0)
+                .environmentObject(questStore)
+        }
+    }
+
+    private func fetchQuest(difficulty: QuestDifficulty) {
+        isLoading = true
+        QuestService.fetchAndStoreQuest(difficulty: difficulty, questStore: questStore, questService: questService) {
+            isLoading = false
+            navigateToHome = true
         }
     }
 }
 
 #Preview {
-    NewQuest()
+    let questStore = QuestStore()
+
+    return NewQuest()
+        .environmentObject(questStore)
 }
